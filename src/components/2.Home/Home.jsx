4a8961addcae7ae3480c2fb5b7619683c71fb1db
dell <xyz@gmail.com>
2022-06-15 17:04:46 +0530

    ok

--- src/components/2.Home/Home.jsx
+++ src/components/2.Home/Home.jsx
@@ -9,17 +9,17 @@ const Home = () => {
             <h1 className='text-center py-md-5 py-3'>Welcome Username !</h1>
             <div className="row py-md-4 py-3 justify-content-center">
 
-                <div className='col-lg-3 col-md-4 col-12 d-flex justify-content-center'>
+                <div className='col-lg-3 col-12 d-flex justify-content-center'>
                     <NavLink to="attendance-management">
                         <button className='btn btn-outline-primary ams-btn'><IoMdFingerPrint size={42} className='mx-2 mb-1' />AMS</button>
                     </NavLink>
                 </div>
-                <div className='col-lg-4 col-md-4 col-12 mt-4 mt-md-0  d-flex justify-content-center'>
+                <div className='col-lg-4 col-12 mt-4 mt-lg-0  d-flex justify-content-center'>
                     <NavLink to="leave-management">
                         <button className='btn btn-outline-danger lms-btn'><BsCalendarWeek size={38} className='mx-3 mb-2' />LMS</button>
                     </NavLink>
                 </div>
-                <div className='col-lg-3 col-md-4 col-12 mt-4 mt-md-0  d-flex justify-content-center'>
+                <div className='col-lg-3 col-12 mt-4 mt-lg-0  d-flex justify-content-center'>
                     <NavLink to="time-sheet">
                         <button className='btn btn-outline-success timesheet-btn'><BsFillFileEarmarkSpreadsheetFill size={38} className='mx-2 mb-2' />Timesheet</button>
                     </NavLink>
